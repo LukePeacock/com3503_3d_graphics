5800da50d5a470f8e9f0787589172faea94255b8
Luke Peacock <lukepeacock13@gmail.com>
2019-10-24 16:51:28 +0100

    starter code

---
+++ Build/Intermediates.noindex/Com3503.build/Debug/Com3503.build/Objects-normal/x86_64/glad.d
@@ -0,0 +1,3 @@
+dependencies: /Users/Luke/Documents/GitHub/temp/Com3503/src/glad.c \
+  /users/luke/documents/rendering/glad/include/glad/glad.h \
+  /users/luke/documents/rendering/glad/include/KHR/khrplatform.h
---
+++ Build/Intermediates.noindex/Com3503.build/Debug/Com3503.build/Objects-normal/x86_64/main.d
@@ -0,0 +1,5 @@
+dependencies: \
+  /Users/Luke/Documents/GitHub/com3503_3d_graphics/source/main.cpp \
+  /users/luke/documents/rendering/glad/include/glad/glad.h \
+  /users/luke/documents/rendering/glad/include/KHR/khrplatform.h \
+  /usr/local/include/GLFW/glfw3.h
